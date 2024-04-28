a05ef34a8c6c20f522c1111b80f544ce2c6a64bd
Walker Milhoan <walker@milhoandesign.com>
2024-04-28 11:50:33 -0600

    Set up

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// Select elements from the DOM
+const expensesInput = document.getElementById('expense');
+const addExpenseBtn = document.getElementById('addExpenseBtn');
+const expensesList = document.getElementById('expensesList');
+const expensesTotal = document.getElementById('totalExpenses');
+
+// Initialize total expenses variable
+let total = 0; 
+
+// Function to update the UI with total expenses
